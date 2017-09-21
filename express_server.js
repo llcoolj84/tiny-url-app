e298aaca1e44e1868cb3e38e2d0ef9cdcce61e21
llcoolj84 <llcj84@gmail.com>
2017-09-21 15:33:48 +0000

    fixed security issues and improved express server

--- express_server.js
+++ express_server.js
@@ -45,7 +45,7 @@ function generateRandomString() {
     return randomString;
 }
 // encrypted cooke functions
-app.set('trust proxy', 1) // trust first proxy
+app.set('trust proxy', 0) // trust first proxy
 
 app.use(cookieSession({
     name: 'session',
@@ -68,20 +68,21 @@ app.get("/urls", (req, res, user_id) => {
     for (i in urlDatabase) {
         if (req.session.user_id === urlDatabase[i].userPoster) {
             filteredDatabase[i] = urlDatabase[i];
+
         } else if (req.session.user_id === undefined) {
             filteredDatabase = urlDatabase;
             break;
         }
     }
     let templateVars = { urls: filteredDatabase, user_id: req.session.user_id };
-    res.render("urls_index", templateVars);
+    return res.render("urls_index", templateVars);
 });
 
 // new tiny URL page
 app.get("/urls/new", (req, res, user_id) => {
     let templateVars = { longURL: urlDatabase[req.params.shorturl], user_id: req.session.user_id }
     if (req.session.user_id !== undefined) {
-        res.render("urls_new", templateVars);
+        return res.render("urls_new", templateVars);
     } else {
         res.send("404 Error. Please login to create a Tiny URL");
     }
@@ -91,37 +92,37 @@ app.get("/urls/new", (req, res, user_id) => {
 app.get("/urls/:shortURL", (req, res, user_id) => {
     let templateVars = { shortURL: req.params.shortURL, targetURL: urlDatabase[req.params.shortURL].fullURL, user_id: req.session.user_id }
     if (req.session.user_id !== undefined) {
-        res.render("urls_show", templateVars);
+        return res.render("urls_show", templateVars);
     }
     res.redirect('/login');
 });
 //  register page
 app.get("/register", (req, res, user_id) => {
     let templateVars = { shortURL: req.params.shortURL, targetURL: urlDatabase[req.params.shortURL], user_id: req.session.user_id }
-    res.render("urls_register", templateVars);
+    return res.render("urls_register", templateVars);
 });
 //  login page
 app.get("/login", (req, res, user_id) => {
     let templateVars = {};
-    res.render("urls_login", templateVars);
+    return res.render("urls_login", templateVars);
 });
 //  register page
 app.post("/register", (req, res, user_id) => {
-    let uEu = req.body.email;
-    let uEp = req.body.password;
+    let userEmail = req.body.email;
+    let userPassword = req.body.password;
     let rString = generateRandomString(); // generate random userid
-    if (uEu == false || uEp == false) {
+    if (userEmail == false || userPassword == false) {
         res.send("404 Error. Enter valid user_id & password");
     }
     users[rString] = {
         id: rString,
-        email: uEu,
-        password: bcrypt.hashSync(uEp, 10)
+        email: userEmail,
+        password: bcrypt.hashSync(userPassword, 10)
     };
     req.session.user_id = users[rString].id;
 
     // res.cookie('email', users[rString].email);
-    res.redirect('/urls');
+    return res.redirect('/urls');
 });
 
 // create new Link page
@@ -136,31 +137,37 @@ app.post("/newLink", (req, res) => {
 });
 //   delete object.property
 app.post("/urls/:id/delete", (req, res) => {
-    delete(urlDatabase[req.params.id]); // delete my object id from the html form
-    res.redirect("http://localhost:8080/urls/"); // redirect to main page
+    if (req.session.user_id === urlDatabase[i].userPoster) {
+        delete(urlDatabase[req.params.id]); // delete my object id from the html form
+        return res.redirect("http://localhost:8080/urls/"); // redirect to main page
+    } else {
+        res.status(403).send('Incorrect User');
+    }
+
 });
+
 //   edit the value of the long url to the new input
 app.post("/urls/:id/update", (req, res) => {
 
     urlDatabase[req.params.id].fullURL = req.body.longURL;
-    res.redirect("http://localhost:8080/urls/"); // redirect to main page
+    return res.redirect("http://localhost:8080/urls/"); // redirect to main page
 });
 
 // login user_id and password create a user cookie
 app.post("/login", (req, res) => { //recieves cookie and redirects
-    let uEu = req.body.email;
-    let uEp = req.body.password;
+    let userEmail = req.body.email;
+    let userPassword = req.body.password;
     let founduser = undefined;
 
     for (id in users) {
-        if (uEu === users[id].email && bcrypt.compareSync(uEp, users[id].password) === true) {
+        if (userEmail === users[id].email && bcrypt.compareSync(userPassword, users[id].password) === true) {
             founduser = id;
         }
     }
 
     if (founduser) {
-        req.session.user_id = id;
-        res.redirect("/urls");
+        req.session.user_id = founduser;
+        return res.redirect("/urls");
     } else {
         res.send("user does not exist");
     }
@@ -170,14 +177,14 @@ app.post("/login", (req, res) => { //recieves cookie and redirects
 // logout user_id and create a user cookie
 app.post("/logout", (req, res) => { //recieves cooking and redirects
     req.session = null;
-    res.redirect("/urls");
+    return res.redirect("/urls");
 });
 // for returning the cookie to display back to user
 app.post("/urls", (req, res) => { //writes user_id cookie to server
     let templateVars = {
         user_id: req.session.user_id
     }
-    res.render("urls_index", templateVars);
+    return res.render("urls_index", templateVars);
 });
 //port message on the console
 app.listen(PORT, () => {
